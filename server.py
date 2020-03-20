ffd1549893d09b190aa035cc974dc1bef558f4cf
benedikt_work <mamontov@smily.ru>
2020-03-20 10:45:37 +0500

    deleted:    hello_you.txt 	modified:   landing.html 	new file:   server.py 	modified:   styles.css

---
+++ server.py
@@ -0,0 +1,26 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class ServerHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print('GET request, path:', self.path)
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        self.wfile.write('answer'.encode('utf-8'))
+
+    def do_POST(self):
+        print('POST request, path:', self.path)
+
+
+def start_server():
+    server_address = ('localhost', 80)
+    srv_inst = HTTPServer(server_address, ServerHandler)
+    try:
+        srv_inst.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    srv_inst.server_close()
+
+if __name__=='__main__':
+    start_server()
